fc1032ca0b688fadb5c1bb7c9bcfec4697eafa25
Cam Moore <cammymoore07@gmail.com>
2015-04-11 10:26:53 -1000

    update bower_components

---
+++ src/main/java/org/wattdepot/extension/openeis/OpenEISLabels.java
@@ -0,0 +1,71 @@
+/*
+ * This file is part of WattDepot.
+ *
+ *  Copyright (C) 2015  Cam Moore
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.wattdepot.extension.openeis;
+
+/**
+ * OpenEISLables - Labels for OpenEIS http://eis.lbl.gov/openeis.html
+ * @author Cam Moore
+ * Created by carletonmoore on 4/11/15.
+ */
+public class OpenEISLabels {
+
+  /** The base label used for OpenEIS analyses. */
+  public static final String OPENEIS = "openeis";
+
+  /** The Time Series Load Profiling label. */
+  public static final String TIME_SERIES_LOAD_PROFILING = "time-series-load-profiling";
+
+  /** The Heat Map label. */
+  public static final String HEAT_MAP = "heat-map";
+
+  /** The Energy Signature label. */
+  public static final String ENERGY_SIGNATURE = "energy-signature";
+
+  /** The Weather Sensitivity label. */
+  public static final String WEATHER_SENSITIVITY = "weather-sensitivity";
+
+  /** The Longitudinal Benchmarking label. */
+  public static final String LONGITUDINAL_BENCHMARKING = "longitudinal-benchmarking";
+
+  /** The Peak Load Benchmarking label. */
+  public static final String PEAK_LOAD_BENCHMARKING = "peak-load-benchmarking";
+
+  /** The Base to Peak Load Ratio label. */
+  public static final String BASE_TO_PEAK_LOAD_RATIO = "base-to-peak-load-ratio";
+
+  /** The Load Duration Curve label. */
+  public static final String LOAD_DURATION_CURVE = "load-duration-curve";
+
+  /** The Load Variability label. */
+  public static final String LOAD_VARIABILITY = "load-variability";
+
+  /** The Power Depository label. */
+  public static final String POWER_DEPOSITORY = "power-depository";
+
+  /** The Temperature Depository label. */
+  public static final String TEMPERATURE_DEPOSITORY = "temperature-depository";
+
+  /** The Power sensor label. */
+  public static final String POWER_SENSOR = "power-sensor";
+
+  /** The Temperature sensor label. */
+  public static final String TEMPERATURE_SENSOR = "temperature-sensor";
+
+}
